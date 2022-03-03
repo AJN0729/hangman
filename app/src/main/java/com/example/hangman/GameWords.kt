ac0b5590ced04a3362167db05ae10da0eb84fb39
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-03-03 09:48:39 -0600

    added code.

---
+++ app/src/main/java/com/example/hangman/GameWords.kt
@@ -0,0 +1,12 @@
+package com.example.hangman
+
+object GameWords {
+    val words = listOf(
+        "Jigglypuff",
+        "Stinky",
+        "Sussy/Baka",
+        "Hogrider",
+        "I'm/not/a/child",
+        ""
+    )
+}
