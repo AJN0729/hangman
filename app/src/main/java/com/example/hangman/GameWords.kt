74177a3071361da9ff154fa15c0bf116f0ebd88a
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-03-03 10:44:11 -0600

    added images and some functions to run the images. they probably won't work yet.

--- app/src/main/java/com/example/hangman/GameWords.kt
+++ app/src/main/java/com/example/hangman/GameWords.kt
@@ -4,9 +4,24 @@ object GameWords {
     val words = listOf(
         "Jigglypuff",
         "Stinky",
+        "Sus",
         "Sussy/Baka",
         "Hogrider",
-        "I'm/not/a/child",
-        ""
+        "I/am/not/a/child",
+        "Kirby",
+        "Poopooegg",
+        "Imposter",
+        "Jigglysuss",
+        "Jigglybuff",
+        "Holypuff",
+        "I/am/your/child",
+        "Sauce",
+        "You/are/the/father",
+        "Chef",
+        "Fossil",
+        "Princess/Diana",
+        "Gordan/Ramsey",
+        "Josh/is/sus",
+        "Breaking/news"
     )
 }
